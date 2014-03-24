bb6d73080673051b4c7c5b1c616104655a6b44ce
Ryuji OCHI <ryuji.ochi@kii.com>
2014-03-24 19:19:04 +0900

    Introdude TSMessage library instead of WBNoticeView.
    - WBNoticeView will hide navigation bar from iOS 7.0. Remove it.
    - Introduce CocoaPods and TSMessage to show success/error message.

--- iOSPushSample/KiiPushListViewController.m
+++ iOSPushSample/KiiPushListViewController.m
@@ -9,8 +9,7 @@
 #import "KiiPushListViewController.h"
 #import "MBProgressHUD.h"
 #import "KiiAppSingleton.h"
-#import "WBStickyNoticeView.h"
-#import "WBSuccessNoticeView.h"
+#import "TSMessage.h"
 #import <KiiSDK/KiiPushInstallation.h>
 #import <KiiSDK/KiiUser.h>
 #import <KiiSDK/Kii.h>
@@ -66,9 +65,8 @@ typedef enum {
     // Close progress
     [MBProgressHUD hideHUDForView:self.view animated:YES];
     if (error == nil) {
-        WBSuccessNoticeView *notice = [WBSuccessNoticeView successNoticeInView:self.view
-                                                                         title:@"Login Successed!!"];
-        [notice show];
+        [TSMessage showNotificationWithTitle:@"Login Successed!!"
+                                        type:TSMessageNotificationTypeSuccess];
     } else {
         message = [NSString stringWithFormat:@"%@", error];
         // Display Login Message
@@ -244,9 +242,8 @@ typedef enum {
                                               otherButtonTitles:nil];
         [messageAlert show];
     } else {
-        WBSuccessNoticeView *notice = [WBSuccessNoticeView successNoticeInView:self.view
-                                                                         title:@"Successed!!"];
-        [notice show];
+        [TSMessage showNotificationWithTitle:@"Successed!!"
+                                        type:TSMessageNotificationTypeSuccess];
     }
 }
 
