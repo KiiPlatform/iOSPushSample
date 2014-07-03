3dffde908c719a19f1c74cf7fe3c4f9e8d951884
Riza <riza.alaudin@kii.com>
2014-07-03 13:39:32 +0900

    Implement uninstallation.

--- iOSPushSample/KiiPushListViewController.m
+++ iOSPushSample/KiiPushListViewController.m
@@ -261,9 +261,8 @@ typedef enum {
 
 // Push uninstallation. But not support this operation.
 - (void)uninstallationPush:(NSError **)error {
-    NSMutableDictionary *errorDetails = [NSMutableDictionary dictionary];
-    [errorDetails setValue:@"This operation not supported." forKey:NSLocalizedDescriptionKey];
-    *error = [NSError errorWithDomain:@"iOSPushSample App" code:999 userInfo:errorDetails];
+    [KiiPushInstallation uninstallSynchronousWithDeviceToken:[KiiAppSingleton sharedInstance].deviceToken
+                                                    andError:error];
 }
 
 // Subscribe bucket
